1a8b27671d937887e55ececb185fd4ff171e93d0
Rany Tarek Bouorm <ranytb@gmail.com>
2024-12-16 12:27:30 +0100

    ci: azure config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build",
+  },
   server: {
     proxy: {
       "/api": {
-        target: "https://localhost:7006",
+        target:
+          "https://redditplaylistgenerator-dev-fubjhzake9adape6.norwayeast-01.azurewebsites.net/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
